d2b5ab803cef9e61e3d3ed9fdd35100c97dc9b42
Dibek <nermin.dibek@Fiserv.com>
2018-02-01 10:56:11 -0500

    Added svn, tortoise and ant to workstation setup

--- WorkstationSetup.ps1
+++ WorkstationSetup.ps1
@@ -1,4 +1,4 @@
 #install Chocolatey and Boxstarter
 . { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
 
-Install-BoxstarterPackage -PackageName https://gist.githubusercontent.com/ndibek/80af91eade925aea40631cd718b6a60f/raw/88b130fa050a84560b1c35ce4c5f7d7a54294a56/challenge-dev -DisableReboots
+Install-BoxstarterPackage -PackageName https://gist.githubusercontent.com/ndibek/80af91eade925aea40631cd718b6a60f/raw/85a21dcc4ad367c3f18dda88fa3de1d6c53d5767/challenge-dev -DisableReboots
